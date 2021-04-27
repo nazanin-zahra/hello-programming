86e174d0eaa30f8e0388f077afabef3957c5d3ad
Nazanin <n.beigirad@gmail.com>
2021-04-27 22:20:06 +0430

    add factorial

---
+++ src/homework/HomeWork20.kt
@@ -0,0 +1,22 @@
+package homework
+
+import java.util.*
+
+object HomeWork20 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+        println("Enter n:")
+        val n = sc.nextInt()
+        val factorial = factorial(n)
+        println("$n! = $factorial")
+
+    }
+
+    fun factorial(m: Int): Int {
+        var sum = 1
+        for (i in 0 until m - 1)
+            sum = (m - i) * sum
+        return sum
+    }
+}
