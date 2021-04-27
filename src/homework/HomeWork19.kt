f92d4108e3a33ccd090bb00561fc71207fdfcd21
Nazanin <n.beigirad@gmail.com>
2021-04-27 21:32:22 +0430

    add home work 19

---
+++ src/homework/HomeWork19.kt
@@ -0,0 +1,32 @@
+package homework
+
+import java.util.*
+
+object HomeWork19 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+        print("Enter m: ")
+        val m = sc.nextInt()
+
+        val primary = isPrimary(m)
+        if (primary) {
+            print("primary")
+        } else {
+            print("not primary")
+        }
+    }
+
+    fun isPrimary(number: Int): Boolean {
+        if (number <= 1) {
+            return false
+        }
+        var mIsPrimary = true
+        for (i in 2 until number) {
+            if (number % i == 0) {
+                mIsPrimary = false
+            }
+        }
+        return mIsPrimary
+    }
+}
