52a5cfc841fbbdc35757a01d9332efad80fbcfdf
Nazanin <n.beigirad@gmail.com>
2021-05-07 21:12:08 +0430

    use && in home work 27

--- src/homework/HomeWork27.kt
+++ src/homework/HomeWork27.kt
@@ -9,10 +9,8 @@ object HomeWork27 {
         val num = mutableListOf<Int>(1, 1, 1, 1)
         for (i in 0 until num.size) {
             for (j in i + 1 until num.size) {
-                if (i < j) {
-                    if (num[i] == num[j]) {
-                        print("($i,$j)")
-                    }
+                if (i < j && num[i] == num[j]) {
+                    print("($i,$j)")
                 }
 
             }
