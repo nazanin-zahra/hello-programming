d92d965592a8968d37f62d7f6b908cd561160878
Nazanin <n.beigirad@gmail.com>
2021-06-02 20:02:09 +0430

    change to two fun home 32

--- src/homework/HomeWork32.kt
+++ src/homework/HomeWork32.kt
@@ -7,17 +7,19 @@ object HomeWork32 {
     // عدد  n  را بگیرد و طول ان را برگرداند .
     @JvmStatic
     fun main(args: Array<String>) {
-        val n = 64783554
+        println(" out:${position(432)}")
+    }
+
+    fun position(n: Int): Int {
         var t = n
         var out = 0
-        val z = t % 10
         while (t > 0) {
-            z
+            t%10
             out += 1
             t = t / 10
 
         }
-        println("Out: $out")
+        return out
     }
 }
 
