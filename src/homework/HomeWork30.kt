e0bcb530d19cd7bcd82f58ee7f0b14bf6e93f296
Nazanin <n.beigirad@gmail.com>
2021-06-02 19:59:36 +0430

    change scanner to two fun home 30

--- src/homework/HomeWork30.kt
+++ src/homework/HomeWork30.kt
@@ -7,16 +7,16 @@ object HomeWork30 {
     // عدد n را گرفته و مجموع ارقام ان را محاسبه کند .
     @JvmStatic
     fun main(args: Array<String>) {
-        val sc = Scanner(System.`in`)
-        print("Enter n: ")
-        val n = sc.nextInt()
+        println("${calculater(123) }")
+    }
+    fun calculater(n:Int):Int{
         var t = n
         var sum = 0
         while (t > 0) {
             sum = sum + (t % 10)
             t = t / 10
         }
-        println("out: $sum")
+        return  sum
     }
 
 }
