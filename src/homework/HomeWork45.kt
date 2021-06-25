84247faf5f0c4a174c811a046a666d741a81acde
Nazanin <n.beigirad@gmail.com>
2021-06-25 19:02:04 +0430

    add home work 45

---
+++ src/homework/HomeWork45.kt
@@ -0,0 +1,28 @@
+package homework
+
+object HomeWork45 {
+    // برنامه ای بنویسید که لیستی را گرفته
+    //و به شکل صعودی مرتب کند
+    //و  سپس لیست مرتب شده را چاپ کند.
+    //(که اعداد تکراری نیستند)
+    //selection sort
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = mutableListOf(-5, 14, 33, 27, 10, 35, 34, 0)
+        for (i in 0 until input.size - 1) {
+            var smallest = i
+            for (j in i + 1 until input.size) {
+                if (input[smallest] > input[j])
+                    smallest = j
+            }
+            if (input[smallest] < input[i]) {
+                val x = input[smallest]
+                input[smallest] = input[i]
+                input[i] = x
+            }
+        }
+        println("out: $input")
+
+    }
+}
+
