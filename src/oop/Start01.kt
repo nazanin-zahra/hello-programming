1bf5258012a02a05c03f67e6432dfe7c16ab79bc
Nazanin <n.beigirad@gmail.com>
2021-07-10 11:51:57 +0430

    add opp1

---
+++ src/oop/Start01.kt
@@ -0,0 +1,34 @@
+package oop
+
+object Start01 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val naziAge: Int = 17
+        val st1: Student = Student("Sara", "Ghornabi", 12)
+        val st2: Student = Student("Nazi", "Beigirad", naziAge)
+        val st3 = Student("Farhad", "Rad", 26)
+//        val studentList: List<Student> = listOf(st1, st2, st3, Student("Ati", 18))
+
+        st1.introduce()
+        st2.introduce()
+
+        println("st3's name is ${st3.name}")
+        println("st3's fullName is ${st3.fullName}")
+    }
+
+    class Student(
+        val name: String,
+        val family: String,
+        val age: Int
+    ) {
+        val fullName = "$name $family"
+
+        init {
+            println("student created with name: $name, age: $age")
+        }
+
+        fun introduce() {
+            println("My name is $name $family. I'm $age years old.")
+        }
+    }
+}
