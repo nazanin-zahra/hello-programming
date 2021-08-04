ce4b8b029c445c68fd22d51a6c3e46aa28fc4709
Nazanin <n.beigirad@gmail.com>
2021-08-04 19:58:09 +0430

    add first part of oop in begineers book website

---
+++ src/oop/Practice1.kt
@@ -0,0 +1,15 @@
+package oop
+
+object Practice1 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+
+    class Examle {
+        var x = 5
+        fun calculater(): Int {
+            return x * x
+        }
+    }
+}
