62d1f73f92e922eb407e4e212a86c1bde3c3d0b0
Nazanin <n.beigirad@gmail.com>
2021-08-05 22:39:07 +0430

    add constructor Any

---
+++ src/oop/ConstructorAny.kt
@@ -0,0 +1,38 @@
+package oop
+
+object ConstructorAny {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val ex1 = Example("A1")
+        val ex2 = Example("A2")
+        val ex3 = Any()
+        val ex4 = Any()
+
+        println("any3 is " + ex3.toString())
+        println("any4 is " + ex4.toString())
+        println("example1 is " + ex1.toString())
+        println("example2 is " + ex2.toString()) // or println("example2 is $ex2")
+    }
+
+    open class Example {
+        var b: String = ""
+
+        constructor(b: String) : super() {
+            this.b = b
+        }
+
+        override fun toString(): String {
+//            return super.toString()
+            return "b = $b"
+        }
+    }
+
+    class ExampleChild : Example {
+        var c: String = ""
+
+        constructor(c: String) : super("B") {
+            this.c = c
+        }
+    }
+}
