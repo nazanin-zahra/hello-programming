a998202709a29b406bedbe245b67adc011608964
Nazanin <n.beigirad@gmail.com>
2021-08-05 22:38:18 +0430

    add topic for practice 4. explain diffrence between "A" & 'A' and a tip for()

--- src/oop/Start05.kt
+++ src/oop/Start05.kt
@@ -18,6 +18,9 @@ object Start05 {
     }
 
     class MyChildClass : MyParentClass() {
+        //constroctor()
+        // if you had written constroctor() then you could omit () which is next to the MyParentClass
+        //but now you didnt mentioned so that you have to write () next to the MyParentClass
         fun myFunction() {
             println(x)  // x is now inherited from the superclass
         }
