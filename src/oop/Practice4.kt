65d38b8178911b745b9a536a29a193f4c1398ede
Nazanin <n.beigirad@gmail.com>
2021-08-05 22:28:11 +0430

    add topic for practice 4. explain diffrence between "A" & 'A'

--- src/oop/Practice4.kt
+++ src/oop/Practice4.kt
@@ -1,18 +1,25 @@
 package oop
-
+// access modifier /visibility modifier
 object Practice4 {
     @JvmStatic
     fun main(args: Array<String>) {
         val obj1 = Parent()
         val obj2 = Child()
+
     }
 
     open class Parent {
         var num = 100
         protected var str = "Beginners book"
-        protected open val ch = 'A' //diffrence with "A"?
+        protected open val ch: Char = 'A' //diffrence with "A"?
+        protected open val ch1: String = "A" //diffrence with "A"?
         internal val number = 99
-        open protected fun demo() {}
+        open protected fun demo() {
+
+        }
+        //protected open fun demo()
+        //no diffrence
+        // you can change these two
     }
 
     class Child : Parent() {
@@ -20,5 +27,8 @@ object Practice4 {
         override fun demo() {
             println("demo function of child class")
         }
+        //  override public fun demo() {
+        // you can not use obj2.demo(). because you dint mentioned it is pubic thets why fun demo is protected by defult
+        //becaused you didnt mention any access modifier
     }
 }
