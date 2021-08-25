ae1ecf605952d62e1c66b5fe22cb87eab94f87b5
Nazanin <n.beigirad@gmail.com>
2021-08-25 19:39:09 +0430

    writting some changes in class Call override fun to string limit type by means of enum class

--- src/sem01/home01/Call.kt
+++ src/sem01/home01/Call.kt
@@ -3,5 +3,15 @@ package sem01.home01
 class Call(
     val createdAt: String,
     val phoneNumber: String,
-    val type: String
-)
+    val type: Type
+) {
+    override fun toString(): String {
+        return "$phoneNumber\t*\t$createdAt\t*\t$type"
+
+    }
+
+    enum class Type {
+        In,
+        Out
+    }
+}
