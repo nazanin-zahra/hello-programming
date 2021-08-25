d47c018a87549c78c217bdc7ad9ff284e791570b
Nazanin <n.beigirad@gmail.com>
2021-08-25 19:45:26 +0430

    writting one change in CallLog class :removing a call by it's phone number instead of throwing exception

--- src/sem01/home01/CallLog.kt
+++ src/sem01/home01/CallLog.kt
@@ -8,16 +8,35 @@ class CallLog {
     }
 
     fun removeCall(phoneNumber: String) {
-        throw Exception("removeCall is not supported now.")
+        for (call in list) {
+            if (phoneNumber == call.phoneNumber) {
+                list.remove(call)
+            }
+
+        }
+//        for (i in 0 until list.size) {
+//            if (phoneNumber == list[i].phoneNumber) {
+//                list.remove(list[i])
+//            }
+//        }
     }
+    // both of them are right but if you do the second you will have problem
+    //cause of the index
+    // so for removing a call in a list ,you should use call except index
+
 
     fun printAll() {
         println("Call log:")
         println("----------------------")
         for (i in 0 until list.size) {
             val call = list[i]
+
 //            println("$i. ${call.phoneNumber}\t${call.type}\t${call.createdAt}")
-            println("$i. ${call.toString()}")
+            println("$i. $call")
+            // u could say call.to string()
+            // writting that is optional
+            // when you write print (call)
+            // it means print call.to string()
         }
     }
 }
