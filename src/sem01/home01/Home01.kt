6f87ea26e8c231f1b1963acc8f46d03cd1eb9644
Nazanin <n.beigirad@gmail.com>
2021-08-25 19:49:36 +0430

    simple changes for Home1

--- src/sem01/home01/Home01.kt
+++ src/sem01/home01/Home01.kt
@@ -4,14 +4,15 @@ object Home01 {
     @JvmStatic
     fun main(args: Array<String>) {
         val log = CallLog()
-        log.addNewCall(Call("2021/11/10", "09121231231", ";ljl;"))
-        log.addNewCall(Call("2021/11/11", "09363456789", "Out"))
-        log.addNewCall(Call("2021/11/11", "09112345678", "In"))
+        log.addNewCall(Call("2021/11/10", "09121231231", Call.Type.In))
+        log.addNewCall(Call("2021/11/11", "09363456789", Call.Type.Out))
+        log.addNewCall(Call("2021/11/11", "09112345678", Call.Type.Out))
 
         log.printAll()
 
-        log.removeCall("09112345678")
+        log.removeCall("09363456789")
         log.printAll()
+
 
     }
 }
