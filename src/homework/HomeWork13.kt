38401956cd70d38dceff8da6615efa5c58db46c9
Nazanin <n.beigirad@gmail.com>
2021-09-25 21:59:20 +0330

    simple and better changes in home work 13

--- src/homework/HomeWork13.kt
+++ src/homework/HomeWork13.kt
@@ -1,7 +1,10 @@
 package homework
 
 import java.util.*
-
+//الگوریتمی بنویسید که عدد طبیعی n را دریافت و
+//ارایه ای به طول n در نظر بگیرد و n مقدار را سوال کند
+//و داخل ان ذخیره کند و سپس مقدار target را سوال نموده
+//و محاسبه کند مقدار target با چند عضو ارایه برابر است.
 object HomeWork13 {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -9,10 +12,12 @@ object HomeWork13 {
         print("Enter n:")
         val n = sc.nextInt()
         val arr = arrayOfNulls<Int>(n)
+        //get number
         for (i in 0..n - 1) {
             print("Enter${i + 1}:")
             arr[i] = sc.nextInt()
         }
+        //finding the numbers which are similar to target
         print("Enter target:")
         val target = sc.nextInt()
         var sum = 0
