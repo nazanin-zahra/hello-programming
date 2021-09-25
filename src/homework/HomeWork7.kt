a866005387a7fc8145204c6b4d3da3e5e2b6cb31
Nazanin <n.beigirad@gmail.com>
2021-09-25 21:57:36 +0330

    simple and better changes in home work 7

--- src/homework/HomeWork7.kt
+++ src/homework/HomeWork7.kt
@@ -12,20 +12,20 @@ object HomeWork7 {
         val one = n % 10
         println("$one")
 
-        val t = n / 10
+        val t = n / 10    //12345
         val ten = t % 10
         println("$ten")
 
-        val b = t / 10
+        val b = t / 10   //1234
         val hundred = b % 10
         println("$hundred")
 
-        val m = b / 10
+        val m = b / 10  //123
         val thousand = m % 10
         println("$thousand")
 
-        val z = m / 10
-        val oneHundredThousand = m / 10
+        val z = m / 10  //12
+        val oneHundredThousand = m / 10   //  12/10=12????????
         println("$oneHundredThousand")
 
     }
