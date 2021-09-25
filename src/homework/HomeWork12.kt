905925c0c6abdc9f012395e3542bac92008ea999
Nazanin <n.beigirad@gmail.com>
2021-09-25 21:58:46 +0330

    simple and better changes in home work 12

--- src/homework/HomeWork12.kt
+++ src/homework/HomeWork12.kt
@@ -1,7 +1,9 @@
 package homework
 
 import java.util.*
-
+//الگوریتمی بنویسید که یک لیست n عنصری زوج را در نظر گرفته باشد
+//و n عدد در ان ذخیره کند .
+//عنصر اول با عنصر اخر؛و عنصر دوم را با عنصر یکی مانده به اخر ؛ جمع کند و نشان دهد.
 object HomeWork12 {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -9,10 +11,11 @@ object HomeWork12 {
         print("Enter n:")
         val n = sc.nextInt()
 
-        if (n % 2 == 1) {
+        if (n % 2 == 1) {  // odd
             println("you must enter an even number")
             main(args)
         } else {
+            //get number
             val arr = arrayOfNulls<Int>(n)
             for (i in 0..n - 1) {
                 print("Enter ${i + 1}:")
