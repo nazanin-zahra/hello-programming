dd306dd3d046dabf9809c0212627ea8d416504d7
Nazanin <n.beigirad@gmail.com>
2021-09-25 21:59:33 +0330

    simple and better changes in home work 14

--- src/homework/HomeWork14.kt
+++ src/homework/HomeWork14.kt
@@ -1,7 +1,12 @@
 package homework
 
 import java.util.*
-
+//الگوریتمی بنویسید که عدد طبیعی n را دریافت
+//و ارایه ای به طول n را در نظر بگیرد و n مقدار را سوال کند
+// و داخل ان ذخیره کند و مقدار target را سوال نموده و
+//بگوید target  چندمین عدد وارد شده است
+//(فرض بر یکتا بودن ورودی هاست)
+//و اگر target بین عدد نبود  بگه؛not found
 object HomeWork14 {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -9,10 +14,12 @@ object HomeWork14 {
         print("Enter n: ")
         val n = sc.nextInt()
         val arr = arrayOfNulls<Int>(n)
+        //get number
         for (i in 0..n - 1) {
             print("Enter $i: ")
             arr[i] = sc.nextInt()
         }
+        //get target to find the index of target in array
         print("Enter target: ")
         val target = sc.nextInt()
         var targetIndex: Int? = null
