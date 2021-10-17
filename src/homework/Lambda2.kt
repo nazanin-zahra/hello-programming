da51917298e44246ecb623b53975995c15791fe8
Farhad Beigirad <f.beigirad@gmail.com>
2021-10-17 22:33:39 +0330

    introduce lambda functions

---
+++ src/homework/Lambda2.kt
@@ -0,0 +1,30 @@
+package homework
+
+object Lambda2 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val a = sum(2, 4)
+        println("sum: $a")
+
+        calculateAndPrint(
+            a = 2,
+            b = 5,
+            calculation = { x, y ->
+                sum(x, y)
+            })
+        calculateAndPrint(
+            a = 2,
+            b = 5,
+            calculation = { x, y ->
+                subtraction(x, y)
+            })
+    }
+
+    fun sum(a: Int, b: Int) = a + b
+    fun subtraction(a: Int, b: Int) = a - b
+
+    fun calculateAndPrint(a: Int, b: Int, calculation: (Int, Int) -> Int) {
+        val result = calculation(a, b)
+        println("result: $result")
+    }
+}
