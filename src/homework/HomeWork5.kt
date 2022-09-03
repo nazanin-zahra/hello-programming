dadb83836c8cad6bf9f3f2ea0e70bd7c2571d462
nazi <n.beigirad@gmail.com>
2022-09-03 21:49:09 +0430

    deleting useless spaces

--- src/homework/HomeWork5.kt
+++ src/homework/HomeWork5.kt
@@ -5,21 +5,13 @@ import java.util.*
 object HomeWork5 {
     @JvmStatic
     fun main(args: Array<String>) {
-      val sc= Scanner(System.`in`)
+        val sc = Scanner(System.`in`)
         print("Enter n:")
-        val n= sc.nextInt()
-        var sum= 0
-        for (w in 6.rangeTo(n) step (6)){
-           sum=sum+w
-
-
-
-
+        val n = sc.nextInt()
+        var sum = 0
+        for (w in 6.rangeTo(n) step (6)) {
+            sum = sum + w
         }
         print("$sum")
-
-
     }
-
-
 }
